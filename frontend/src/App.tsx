713a525e272a0df53029eaa6a2560a012b5fc2f2
Melanie Reinoso <melaniereinoso3103@gmail.com>
2025-11-28 11:55:17 -0600

    agregar importacion al home.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,20 +7,18 @@ import UneteTecnico from "./components/UneteTecnico";
 import Footer from "./components/Footer";
 import FAQ from "./components/FAQ";
 import TecnicoTarjeta from "./pages/catalogo";
+import AcercaDe from "./components/acercade";
 
 function App() {
   return (
-
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/catalogo" element={<TecnicoTarjeta/>}/>
+        <Route path="/catalogo" element={<TecnicoTarjeta />} />
       </Routes>
 
-
-    
       <Footer />
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import ComoFunciona from "../components/ComoFunciona";
+import AcercaDe from "../components/acercade";
 import FAQ from "../components/FAQ";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
@@ -7,14 +8,13 @@ import ServicesSection from "../components/ServicesSection";
 import TopTechniciansSection from "../components/TopTechniciansSection";
 import UneteTecnico from "../components/UneteTecnico";
 
-
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
       <SearchForm />
-        <main className="home-main">
+      <main className="home-main">
         <ServicesSection />
         <TopTechniciansSection />
       </main>
